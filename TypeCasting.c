0bfd1e284db4964f714a217f5f8728cdd94b5a2d
dgndrn <celaldoganduran@gmail.com>
2023-08-08 15:51:19 +0300

    Update TypeCasting.c

--- TypeCasting.c
+++ TypeCasting.c
@@ -21,3 +21,27 @@ string veli = "string";
 
 
 }
+
+
+
+
+
+
+inline std::wstring convert(const std::string& as)  //verinin stringden wstringe donusturulmesi
+ {
+	// deal with trivial case of empty string
+	if (as.empty())    return std::wstring();
+
+	// determine required length of new string
+	size_t reqLength = ::MultiByteToWideChar(CP_UTF8, 0, as.c_str(), (int)as.length(), 0, 0);
+
+	// construct new string of required length
+	std::wstring ret(reqLength, L'\0');
+
+	// convert old string to new string
+	::MultiByteToWideChar(CP_UTF8, 0, as.c_str(), (int)as.length(), &ret[0], (int)ret.length());
+
+	// return new string ( compiler should optimize this away )
+	return ret;
+}
+
