bfa0796255a1b4bafafe7d2be403c6916d1bb9ba
dgndrn <celaldoganduran@gmail.com>
2023-08-22 10:51:17 +0300

    .

--- HttpRequest/HttpRequest/get.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include"lib.h"
-int get_request();
--- HttpRequest/HttpRequest/lib.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <wininet.h>
-#include <iostream>
-#include"get.h"
-#include"post.h"
-using namespace std;
-#pragma comment (lib, "Wininet.lib")
-#pragma warning(disable:4996)
